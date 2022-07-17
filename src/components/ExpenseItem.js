b27a27618c1cea7391d8edf95bde2bb2d560e345
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-17 15:23:06 +0600

    setup project for expense tracker

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(props){
+    return (
+        <div>
+            <div> <h2>22 feb 2022</h2></div>
+            <div> <h2>This is title</h2></div>
+            <div> <h2>$244.42</h2></div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
