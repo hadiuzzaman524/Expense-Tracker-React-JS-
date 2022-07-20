703d234d985c21e327777daa6176cfe6ea5d8feb
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-20 20:26:23 +0600

    design card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
-     <ExpenseItem></ExpenseItem>
+      <ExpenseItem date="22 feb 2022" title="Buy T-shirt" />
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
-function ExpenseItem(props){
-    return (
-        <div>
-            <div> <h2>22 feb 2022</h2></div>
-            <div> <h2>This is title</h2></div>
-            <div> <h2>$244.42</h2></div>
-        </div>
-    );
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  
+  return (
+    <div className="expense-item">
+        <h2>22 feb 2022</h2>
+      <div className="expense-item__description">
+        <h2>This is title</h2>
+      </div>
+      <div className="expense-item__price">
+        <h2>$244.42</h2>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
