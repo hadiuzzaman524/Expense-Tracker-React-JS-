f925322ba6fe8da323ee68a107d09278354cc602
Md Hadiuzzaman <hadi@heavytask.com>
2022-07-21 16:54:34 +0600

    learn about Date & props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   
   return (
     <div className="expense-item">
-        <h2>22 feb 2022</h2>
+      <div><ExpenseDate date= {props.date}></ExpenseDate></div> 
       <div className="expense-item__description">
-        <h2>This is title</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">
-        <h2>$244.42</h2>
+        <h2>{props.amount}</h2>
       </div>
     </div>
   );
